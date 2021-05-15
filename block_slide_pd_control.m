db3492f98153166af3f6746cebceaa87bff5f973
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-05-14 23:16:49 -0400

    updated comments, etc. in block slide sim

--- block_slide_pd_control.m
+++ block_slide_pd_control.m
@@ -15,24 +15,25 @@ videoFrameRate = 30; % [frames/sec]
 
 % simulation time parameters    
 t0 = 0;         % [s] simulation start time
-tf = 4;         % [s] simulation end time
+tf = 2;         % [s] simulation end time
 dt = 0.015;     % [s] timestep size
 opts = odeset('RelTol',1e-8,'AbsTol',1e-12);
 
-% initial conditions (state vector: [x x_dot theta theta_dot]')
-x_0         = 0;  % [m]
-x_dot_0     = 0;  % [m/s]
-X0 = [x_0 x_dot_0]'; % [m m/s]'
+% initial conditions (state vector: [x x_dot]')
+x_0         = 0;      % [m]
+x_dot_0     = 0;      % [m/s]
+X0 = [x_0 x_dot_0]';  % [m m/s]'
 X = X0;
 
 % system parameters
-sysParams.m   = 2;   % [kg] cart mass
-sysParams.kp  = 300; % [N/m] proportional gain
-sysParams.kd  = 150; % [Ns/m] derivative gain
-sysParams.F   = 0;   % [N] computed control force to be applied
+sysParams.m   = 2;         % [kg] cart mass
+sysParams.kp  = 300;       % [N/m] proportional gain
+sysParams.kd  = 150;       % [Ns/m] derivative gain
+sysParams.F   = 0;         % [N] computed control force to be applied
 sysParams.w0  = 2*pi*1.0;  % [rad/s] driving frequency 
-sysParams.xamp = 0.5; % [m] amplitude of sinusoidal target position (assumse start from negative side)
-sysParams.Fmax = 50; % [N] saturate force at this level
+sysParams.xamp = 0.5;      % [m] amplitude of sinusoidal target position (assumse start from negative side)
+sysParams.Fmax = 50;       % [N] saturate force at this level
+
 % data storage
 time = [t0];
 data = [X0];
@@ -43,7 +44,8 @@ for t = t0:dt:(tf-dt)
     % calculate timestep for ODE solving
     odeTime = [t t+dt];
     
-    % COULD SIMPLIFY THIS BY STARTING FROM ONE END AND USING 1-COS(W*T) FOR POSITION
+    % COULD SIMPLIFY THIS BY STARTING FROM ONE END AND USING 1-COS(W*T) FOR
+    % POSITION AND AVOID TRANSIENT!
     x_target = sysParams.xamp*sin(sysParams.w0*odeTime(2));
     v_target = sysParams.xamp*sysParams.w0*cos(sysParams.w0*odeTime(2));  
 %     x_target = sysParams.xamp*(1-cos(sysParams.w0*odeTime(2)));
@@ -64,44 +66,33 @@ for t = t0:dt:(tf-dt)
     data(:,end+1) = X; % note: discarding state values at intermediate timesteps calculated by ode45()
 end
 
-%% compute total energy in system
+%% show trajectory
 x = data(1,:);
 x_dot = data(2,:);
-% theta = data(3,:);
-% theta_dot = data(4,:);
-% ma = sysParams.ma;
-% mb = sysParams.mb;
-% l = sysParams.l;
-% g = sysParams.g;
-% T = 0.5*ma*x_dot.^2 + 0.5*mb*(x_dot + l*theta_dot.*cos(theta)).^2 + 0.5*mb*(l*theta_dot.*sin(theta)).^2;
-% V = mb*g*l*(1-cos(theta));
-% E = T+V;
-% 
-% % plot total energy
+
 figure;
-subplot(4,1,1);
+subplot(3,1,1);
 hold on; grid on;
 plot(time,x,'-','LineWidth',1.6);
 plot(time,sysParams.xamp*sin(sysParams.w0*time),'--','Color',[0.7 0 0],'LineWidth',2);
 % plot(time,sysParams.xamp*(1-cos(sysParams.w0*time)),'--','Color',[0.7 0 0],'LineWidth',2);
+xlabel('\bfTime [sec]');
+ylabel('\bfx [m]');
 
-subplot(4,1,2);
+subplot(3,1,2);
 hold on; grid on;
 plot(time,x_dot,'-','LineWidth',1.6);
 plot(time,sysParams.xamp*sysParams.w0*cos(sysParams.w0*time),'--','Color',[0.7 0 0],'LineWidth',2);
+xlabel('\bfTime [sec]');
+ylabel('\bfx\_dot [m]');
 
-subplot(4,1,3);
+subplot(3,1,3);
 hold on; grid on;
 plot(time,gradient(x_dot,time),'-','LineWidth',1.6);
 plot(time,-1*sysParams.xamp*(sysParams.w0^2)*sin(sysParams.w0*time),'--','Color',[0.7 0 0],'LineWidth',2);
+xlabel('\bfTime [sec]');
+ylabel('\bfx\_ddot [m]');
 
-
-
-% plot(time,E-E(1),'-','LineWidth',1.6,'Color',[0 0.7 0]);
-% xlabel('\bfTime [sec]');
-% ylabel('\bfΔ Energy [J]');
-% title('\bfEnergy Check','FontSize',12);
-% 
 %% animate result in a new plot
 
 % initialize figure
@@ -118,7 +109,7 @@ cart.f = [1 2 3 4 1];
 % plot system
 plot([-3 3],[0 0],'k-','LineWidth',2);
 ph_cart  = patch('Faces',cart.f,'Vertices',nan(size(cart.v)),'FaceColor','flat','EdgeColor','k','LineWidth',2,'FaceColor',0.7*ones(1,3));
-ph_pivot = plot(nan,0.075,'k.','MarkerSize',50);
+ph_pivot = plot(nan,0.075,'k.','MarkerSize',30);
 
 % finalize plot
 ph_title = title('');
@@ -172,8 +163,8 @@ x         = X(1);
 x_dot     = X(2);
 
 % construct Xdot from differential equation
-% note:     X    = [x x_dot theta theta_dot]'
-% therefore Xdot = [x_dot x_ddot theta_dot theta_ddot]'
+% note:     X    = [x x_dot]'
+% therefore Xdot = [x_dot x_ddot]'
 Xdot = zeros(2,1);
 Xdot(1,:) = x_dot;
 Xdot(2,:) = F/m;
